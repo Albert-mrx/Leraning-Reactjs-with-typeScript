c5cb79905630fc1a962288192c33a69e3aeb940c
=Albert <dliktans@gmail.com>
2026-03-19 23:45:24 -0500

    use ref para manejo  de cache

--- src/gifs/hooks/useGifs.tsx
+++ src/gifs/hooks/useGifs.tsx
@@ -1,19 +1,20 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import type { Gif } from "../interfaces/gif.interface";
 import { getGifsByQuery } from "../actions/get-gifs-by-query.action";
-const gifsCache:Record<string,Gif[]> = {};
+
+// const gifsCache:Record<string,Gif[]> = {};
 
 
 export const useGifs = ()=>{
         const [gifs,setGifs] = useState<Gif[]>([])
         const [previousTerms, setPreviousTerms] = useState<string[]>([]);
 
-        
+        const gifsCache = useRef<Record<string,Gif[]>>({});
 
         const handleTermClicked = async (term:string)=>{
            
-            if(gifsCache[term]){
-                setGifs(gifsCache[term]);
+            if(gifsCache.current[term]){
+                setGifs(gifsCache.current[term]);
                 return;
             }
             
@@ -28,7 +29,7 @@ export const useGifs = ()=>{
 
             const gifs = await getGifsByQuery(query);
             setGifs(gifs);
-            gifsCache[query]=gifs;
+            gifsCache.current[query]=gifs;
 
             console.log(gifsCache);
             // const cleanedQuery = query.toLowerCase().trim();
